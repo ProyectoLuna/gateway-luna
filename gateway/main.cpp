f2ee3b2cc5364bd1758b6b4922bbb43e60ea7af4
Miguel Garcia <mgm.mgm.85@gmail.com>
2017-10-15 23:04:38 +0200

    Modified gitignore

--- gateway/main.cpp
+++ gateway/main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include "radiorf24.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    qDebug() << "Soy una raspberry";
+
+    RadioRF24 radiorf24;
+    radiorf24.check_remotes();
     return 0;
 }
