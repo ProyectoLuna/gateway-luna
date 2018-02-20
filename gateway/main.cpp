f455dd132b6644ed8c7b8f6d342fc0d6717e895f
Miguel Garcia <mgm.mgm.85@gmail.com>
2018-02-20 21:50:02 +0100

    Added nanopb library and remote device proto message.

--- gateway/main.cpp
+++ gateway/main.cpp
@@ -11,6 +11,13 @@
 #include "radio_manager.h"
 #include "daemon.h"
 
+extern "C"
+{
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "remote_devs.pb.h"
+}
+
 using namespace luna;
 
 int main(int argc, char *argv[])
@@ -58,6 +65,50 @@ int main(int argc, char *argv[])
             e.what();
             exit(1);
         }
+    }
+
+    uint8_t buffer[256];
+    size_t message_length;
+    bool status;
+
+    {
+        RemoteDevMessage message = RemoteDevMessage_init_zero;
+
+        pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+        message.header.transaction_id = 1;
+        message.header.unique_id.radio_id = RadioId_RID_NRF24;
+
+        // TODO investigar el repeated
+
+        status = pb_encode(&stream, RemoteDevMessage_fields, &message);
+        message_length = stream.bytes_written;
+
+        /* Then just check for any errors.. */
+        if (!status)
+        {
+            qInfo() << "Encoding failed: " << PB_GET_ERROR(&stream);
+            return EXIT_FAILURE;
+        }
+    }
+
+    {
+        RemoteDevMessage message = RemoteDevMessage_init_zero;
+
+        /* Create a stream that reads from the buffer. */
+        pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
+
+        status = pb_decode(&stream, RemoteDevMessage_fields, &message);
+
+        if (!status)
+        {
+            qInfo() << "Decoding failed: " << PB_GET_ERROR(&stream);
+            return 1;
+        }
+
+        /* Print the data contained in the message. */
+        LOG_INFO(QString("ID: %1, transaction: %2").arg(message.header.unique_id.radio_id).
+                                                    arg(message.header.transaction_id));
     }
 
     RadioManager radioManager;
